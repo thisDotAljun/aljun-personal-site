6263d78a139a16bcb8d3caf7cb9d4a99b6d264c0
thisDotAljun <cortinaaljun@gmail.com>
2025-08-03 19:23:43 +0800

    ui:Develop Technical Skills Section
    - Create component for project images and description
    - Create static data to loop the project for maintainability
    - Create file for base layouts

---
+++ src/components/technicalskills/ImagesShots.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card class="w-fit m-auto">
+    <template #content>
+      <Image :src="src" alt="Image" pt:image:class="w-[300px] h-[200px] rounded-lg" />
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Image from 'primevue/image'
+import Card from 'primevue/card'
+
+type ScreenShotImageProps = {
+  src: string
+}
+
+defineProps<ScreenShotImageProps>()
+</script>
---
+++ src/components/technicalskills/ProjectDescription.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="relative flex flex-col justify-center w-full max-w-9/12 gap-5">
+    <h1 class="font-medium md:ml-auto leading-none">{{ projectName }}</h1>
+    <div class="inline-flex flex-wrap justify-start md:justify-end gap-2">
+      <Chip
+        v-for="(language, key) in languageUse"
+        :key="key"
+        :label="language"
+        class="bg-primary-black text-white !text-xs"
+      />
+    </div>
+    <p class="">
+      {{ description }}
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Chip from 'primevue/chip'
+
+type SkillDescriptionProps = {
+  projectName: string
+  description: string
+  languageUse: string[]
+}
+
+defineProps<SkillDescriptionProps>()
+</script>
