3a02e8028f522c053dcf9bbf92868c6908a4f3b4
thisDotAljun <cortinaaljun@gmail.com>
2025-08-03 19:24:57 +0800

    ui:Update Min Layout
    - Re-arrange the component structure and apply base resposive on mobile first approach

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <HeroLayout />
+    <TechSkillsLaylout />
+  </main>
+</template>
+
+<script setup lang="ts">
+import HeroLayout from './HeroLayout.vue'
+import TechSkillsLaylout from '@/layout/TechSkillsLaylout.vue'
+</script>
