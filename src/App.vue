cd86aab7a4c6eb2df50ced8b93e917c97ead61db
thisDotAljun <cortinaaljun@gmail.com>
2025-08-03 12:32:56 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-red-500">Hi</h1>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import './assets/tailwind/index.css'
+import 'primeicons/primeicons.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
+import { ConfirmationService, KeyFilter, Ripple, ToastService, Tooltip } from 'primevue'
+
+const DefaultPreset = definePreset(Aura, {
+  semantic: {
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{Sky.600}',
+          inverseColor: '#ffffff',
+          hoverColor: '{Sky.700}',
+          activeColor: '{Sky.500}',
+        },
+      },
+    },
+  },
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: DefaultPreset,
+    options: {
+      darkModeSelector: false,
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
+app.use(ConfirmationService)
+app.use(ToastService)
+
+app.directive('tooltip', Tooltip)
+app.directive('ripple', Ripple)
+app.directive('keyfilter', KeyFilter)
+
+app.mount('#app')
